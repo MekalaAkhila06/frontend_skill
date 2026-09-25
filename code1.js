a8e02e84b840e7669099c47f6f82e365d633ab43
Akhila <mekalaakhila.06@gmail.com>
2026-09-25 10:11:01 +0530

    Add factorial JavaScript program

---
+++ code1.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter a number: ", function(n) {
+    n = parseInt(n);
+    let factorial = 1;
+
+    for (let i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    console.log("Factorial of " + n + " is " + factorial);
+
+    rl.close();
+});
